9f76d72b44dafd7024091927c906aa68e4389cd7
Mike Zimmer <mike.zimmer@thieleundklose.de>
2023-10-13 10:31:25 +0200

    adjust extension information

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,12 +3,13 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Thiele & Klose - Autotranslate',
     'description' => 'Integration of automatic translations into the backend process when maintaining pages and content elements by editors.',
-    'category' => 'plugin',
-    'author' => '',
-    'author_email' => '',
+    'category' => 'be',
+    'author' => 'Mike Zimmer',
+    'author_company' => 'Thiele & Klose GmbH',
+    'author_email' => 'typo3@thieleundklose.de',
     'state' => 'beta',
     'clearCacheOnLoad' => 0,
-    'version' => '0.2.1',
+    'version' => '0.9.1',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.5.99',
