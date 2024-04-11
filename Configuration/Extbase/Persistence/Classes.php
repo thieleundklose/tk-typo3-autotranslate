d54db44ffecd308c82f0eaee78c5eefbea8d21ad
Mike Zimmer <mike.zimmer@thieleundklose.de>
2024-04-11 14:28:05 +0200

    TK3-57 - Erstellung Repository und Model
    - add table mapping

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    \ThieleUndKlose\Autotranslate\Domain\Model\BatchItem::class => [
+        'tableName' => 'tx_autotranslate_batch_item',
+    ],
+];
