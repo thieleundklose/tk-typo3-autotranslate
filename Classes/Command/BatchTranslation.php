0ca03a5498a83dc0fcee2fab3941c10eeac5afc4
Tamara Glöckner <tamara.gloeckner@Tamaras-MacBook-Pro.local>
2024-04-15 11:42:44 +0200

    TK3-56 - [T3D] DeepL Translator - Erstellen eines Symfony Commands inkl. optionaler Parameter
    - modify command
    - extend command output

--- Classes/Command/BatchTranslation.php
+++ Classes/Command/BatchTranslation.php
@@ -10,32 +10,31 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-final class Autotranslate extends Command
+final class BatchTranslation extends Command
 {
     protected function configure(): void
     {
         $this
+        ->setDescription(
+            'A command for automatic translation with feedback. 
+            Optionally add the number of translations which is otherwise 1 per default.'
+        )
         ->setHelp('Optionally add the number of translations which is otherwise 1 per default.')
         ->addArgument(
             'translationNumber',
             InputArgument::OPTIONAL,
-            'A number for keeping track of the amount of translations',
+            'Enter the number of translations',
             '1'
-        )
-        ->addOption(
-            'translations',
-            't',
-            InputOption::VALUE_NONE,
-            'A command for automatic translations',
         );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $translate=(bool)$input->getOption('translations');
-        if ($translate) {
-            $output->writeln($input->getArgument('translationNumber'));
-        }
+        $translationNumber = $input->getArgument('translationNumber');
+        $translationNumber = (int) $translationNumber;
+        $successfulTranslations = rand(0, $translationNumber);
+        $output->writeln($successfulTranslations.' translation(s) completed successfully!');
+        $output->writeln(($translationNumber - $successfulTranslations).' translation(s) failed.');
         return Command::SUCCESS;
     }
 }
