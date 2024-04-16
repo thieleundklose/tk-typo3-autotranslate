5590ce7f3ec0386e4da6dc8fc50efc17ba467c1d
Mike Zimmer <mike.zimmer@thieleundklose.de>
2024-04-16 09:53:52 +0200

    TK3-56 - [T3D] DeepL Translator - Erstellen eines Symfony Commands inkl. optionaler Parameter
    - review changes

--- Classes/Command/BatchTranslation.php
+++ Classes/Command/BatchTranslation.php
@@ -7,34 +7,42 @@ namespace ThieleUndKlose\Autotranslate\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class BatchTranslation extends Command
 {
+
+    /**
+     * @return void
+     */
     protected function configure(): void
     {
         $this
-        ->setDescription(
-            'A command for automatic translation with feedback. 
-            Optionally add the number of translations which is otherwise 1 per default.'
-        )
-        ->setHelp('Optionally add the number of translations which is otherwise 1 per default.')
-        ->addArgument(
-            'translationNumber',
-            InputArgument::OPTIONAL,
-            'Enter the number of translations',
-            '1'
-        );
+            ->setDescription(
+                'A command for automatic translation with feedback. 
+                Optionally add the number of translations which is otherwise 1 per default.'
+            )
+            ->setHelp('Optionally add the number of translations which is otherwise 1 per default.')
+            ->addArgument(
+                'translationsPerRun',
+                InputArgument::OPTIONAL,
+                'Enter the number of translations you want to perform in a run. Default is 1.',
+                '1'
+            );
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $translationNumber = $input->getArgument('translationNumber');
-        $translationNumber = (int) $translationNumber;
-        $successfulTranslations = rand(0, $translationNumber);
-        $output->writeln($successfulTranslations.' translation(s) completed successfully!');
-        $output->writeln(($translationNumber - $successfulTranslations).' translation(s) failed.');
+        $translationsPerRun = $input->getArgument('translationsPerRun');
+        $translationsPerRun = (int)$translationsPerRun;
+        $successfulTranslations = rand(0, $translationsPerRun);
+        $output->writeln($successfulTranslations . ' translation(s) completed successfully!');
+        $output->writeln(($translationsPerRun - $successfulTranslations) . ' translation(s) failed.');
         return Command::SUCCESS;
     }
 }
