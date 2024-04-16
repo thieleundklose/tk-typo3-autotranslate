befffd57a9cc0a6d8dd88d217c507be242fea802
Florian GRamsch <florian.gramsch@thieleundklose.de>
2024-04-16 16:43:36 +0200

    TK3-58 - [T3D] DeepL Translator - Logging des Symfony Command
    Implementation of a LoggerInterface

--- Classes/Command/BatchTranslation.php
+++ Classes/Command/BatchTranslation.php
@@ -9,12 +9,36 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use Psr\Log\LoggerInterface;
+use TYPO3\CMS\Core\Log\Channel;
+
 final class BatchTranslation extends Command
 {
 
     /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
      * @return void
      */
+
+    public function __construct()
+    {
+        $GLOBALS['TYPO3_CONF_VARS']['LOG']['ThieleUndKlose']['Autotranslate']['Command']['BatchTranslation'] = [
+            'writerConfiguration' => [
+                \Psr\Log\LogLevel::INFO => [
+                    \TYPO3\CMS\Core\Log\Writer\DatabaseWriter::class => [
+                        'logTable' => 'sys_log',
+                    ],
+                ],
+            ],
+        ];
+        $this->logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Log\LogManager::class)->getLogger('ThieleUndKlose.Autotranslate.Command.BatchTranslation');
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
         $this
@@ -41,6 +65,25 @@ final class BatchTranslation extends Command
         $translationsPerRun = $input->getArgument('translationsPerRun');
         $translationsPerRun = (int)$translationsPerRun;
         $successfulTranslations = rand(0, $translationsPerRun);
+
+        $this->logger->info('{completed} Tasks completed and {failed} Tasks failed', [
+            'completed' => $successfulTranslations,
+            'failed' => $translationsPerRun - $successfulTranslations,
+        ]);
+
+        // $GLOBALS['BE_USER']->writeLog(
+        //     4,  // Type (4 = message)
+        //     0,  // Action (0 = message)
+        //     0,  // Error level (0 = OK)
+        //     0,  // Details number
+        //     '{completed} Tasks completed and {failed} Tasks failed',  // Details
+        //     [
+        //         'completed' => $successfulTranslations,
+        //         'failed' => $translationsPerRun - $successfulTranslations,
+        //     ],  // Data
+        //     'channel' => 'test'
+        // );
+
         $output->writeln($successfulTranslations . ' translation(s) completed successfully!');
         $output->writeln(($translationsPerRun - $successfulTranslations) . ' translation(s) failed.');
         return Command::SUCCESS;
