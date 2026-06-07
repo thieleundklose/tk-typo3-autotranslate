f5782e2d9c4d3836b104100815030957cb0fbd47
Mike Zimmer <mike.zimmer@thieleundklose.de>
2026-06-07 16:39:34 +0200

    feat: cleanup

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,8 +16,8 @@ $EM_CONF[$_EXTKEY] = [
         ],
         'conflicts' => [],
         'suggests' => [
-            'news' => '10.0.0-10.99.99',
-            'deepltranslate_glossary' => '5.0.0-5.0.0',
+            'news' => '11.0.0-14.99.99',
+            'deepltranslate_glossary' => '5.0.0-5.99.99',
         ],
     ],
     'autoload' => [
