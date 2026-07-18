b3768e5bd8e8c70ae8ac01d95edbc49d10e88fe3
Mike Zimmer <mike.zimmer@thieleundklose.de>
2026-07-18 09:04:20 +0200

    fix pr 143 for typo3 v11 usage

--- Classes/TsConfig/Loader.php
+++ Classes/TsConfig/Loader.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace ThieleUndKlose\Autotranslate\TsConfig;
 
 use TYPO3\CMS\Core\Configuration\Event\ModifyLoadedPageTsConfigEvent as LegacyModifyLoadedPageTsConfigEvent;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\TypoScript\IncludeTree\Event\ModifyLoadedPageTsConfigEvent;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use ThieleUndKlose\Autotranslate\Utility\TranslationHelper;
@@ -33,19 +34,30 @@ class Loader
         // Business code
         $siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
         $sites = $siteFinder->getAllSites();
+        $typo3MajorVersion = GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion();
 
         foreach ($sites as $site) {
+            $rootPageId = (int)$site->getRootPageId();
             foreach (TranslationHelper::tablesToTranslate() as $table) {
                 $settings = TranslationHelper::translationSettingsDefaults($site->getConfiguration(), $table);
                 if (!$settings) {
                     continue;
                 }
                 $event->addTsConfig('
-                    [site("rootPageId") == ' . $site->getRootPageId() . ']
+                    [' . $this->buildSiteRootCondition($rootPageId, $typo3MajorVersion) . ']
                         TCAdefaults.' . $table . '.autotranslate_languages = ' . $settings['autotranslateLanguages'] . '
                     [end]
                 ');
             }
         }
+    }
+
+    private function buildSiteRootCondition(int $rootPageId, int $typo3MajorVersion): string
+    {
+        if ($typo3MajorVersion <= 11) {
+            return 'traverse(page, "uid") == ' . $rootPageId . ' || ' . $rootPageId . ' in tree.rootLineParentIds';
+        }
+
+        return 'site("rootPageId") == ' . $rootPageId;
     }
 }
